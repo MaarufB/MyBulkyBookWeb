7fdfb38e84851c1fc6be314a5ca8f82128e02a5f
MaarufB <maarufburad1231@gmail.com>
2022-01-29 22:01:21 +0800

    create main branch

--- BulkyBook.DataAccess/Repository/CategoryRepository.cs
+++ BulkyBook.DataAccess/Repository/CategoryRepository.cs
@@ -1,10 +1,6 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace BulkyBook.DataAccess.Repository
 {
--- BulkyBook.DataAccess/Repository/IRepository/ICategoryRepository.cs
+++ BulkyBook.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -1,9 +1,5 @@
 ﻿using BulkyBook.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace BulkyBook.DataAccess.Repository.IRepository
 {
